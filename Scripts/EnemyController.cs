93d5de0637252d0bb8e36c9c98ed02fba2b7c14a
toruSIF378 <49849074+toruSIF378@users.noreply.github.com>
2019-11-15 13:39:20 +0900

    abc

---
+++ Scripts/EnemyController.cs
@@ -0,0 +1,76 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityStandardAssets.Characters.ThirdPerson;
+[RequireComponent(typeof(UnityEngine.AI.NavMeshAgent))]
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class EnemyController : MonoBehaviour
+{
+    [System.NonSerialized]
+    public bool isActive = false;
+    [SerializeField]
+    Transform target;
+    NavMeshAgent agent;
+    ThirdPersonCharacter character;
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+        character = GetComponent<ThirdPersonCharacter>();
+        agent.updateRotation = false;
+        agent.updatePosition = true;
+    }
+    private void Update()
+    {
+        if (isActive)
+        {
+            agent.isStopped = false;
+            if (target != null)
+                agent.SetDestination(target.position);
+            if (agent.remainingDistance > agent.stoppingDistance)
+            {
+                character.Move(agent.desiredVelocity, false, false);
+            }
+            else
+            {
+                character.Move(Vector3.zero, false, false);
+            }
+        }
+        else
+        {
+            character.Move(Vector3.zero, false, false);
+            agent.isStopped = true;
+            return;
+        }
+
+    }
+    public void SetTarget(Transform target)
+    {
+        this.target = target;
+    }
+    //他のオブジェクトに接触したときの処理
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            Debug.Log("ゲームオーバーです。");
+            isActive = false;
+        }
+    }
+    //検出範囲に入った時の処理
+    void OnTriggerStay(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isActive = true;
+        }
+    }
+    //検出範囲から出たときの処理
+    void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            isActive = false;
+        }
+    }
+}
